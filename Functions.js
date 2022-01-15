2dbd0d932691456a6ed9ba33e67efcb2e67715c5
Anthony Malary <amalary@bu.edu>
2022-01-14 17:08:09 -0800

    Planet has water function

--- Functions.js
+++ Functions.js
@@ -50,3 +50,38 @@ const add = (a,b) => a + b ;
 
 console.log(add(23,25)); 
 
+// Calling Functions 
+
+const add = (a,b) => a + b ; 
+
+console.log(add(100,25)); 
+
+function getRandomInt(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+};
+
+console.log(getRandomInt(10,20));
+
+// The area of a rectangle with a width of ___ and a height of ___ is ___ square units
+
+function computeArea(width,height){
+
+    let area = width * height
+
+    return `The area of a rectangle with a width of ${width} and a height of ${height} is ${area} square units`
+
+}
+
+console.log(computeArea(5,25));
+
+const planetHasWater = function (planet){
+
+    if(planet === 'Earth' || planet === 'Mars'){
+        return true
+    }
+    else {
+        return false
+    };
+}; 
+
+console.log(planetHasWater('mars')); 
