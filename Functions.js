6dd9364bb1d3eb42d2ebffb8e7737aa1e81a437f
Anthony Malary <amalary@bu.edu>
2022-01-15 17:59:27 -0800

    Functions is done with

--- Functions.js
+++ Functions.js
@@ -84,5 +84,85 @@ const planetHasWater = function (planet){
     };
 }; 
 
-console.log(planetHasWater('MaRs')); 
+console.log(planetHasWater('MaRs'));  
 
+// Default Parameters 
+
+function setColor(bicycle = "A really nice brand", color = "red"){
+    return bicycle.color = color; 
+
+};
+
+console.log(setColor())
+
+
+function multiplyNums(a= 5, b= 32){
+    return a * b
+}; 
+
+console.log(multiplyNums(12,23)); 
+
+console.log(multiplyNums()); 
+
+let a = ['red','green','blue']; 
+
+a.forEach(function (color){
+    console.log(color); 
+})
+
+// Return Statement 
+
+const addNums = function (num1 = 10,num2 = 123) {
+    if(typeof num1 === 'string' || typeof num2 === 'string'){
+
+        return "let's get serious people";
+    }
+    return num1 + num2;
+};
+
+console.log(addNums('fifteen',12)); 
+
+console.log(addNums()); 
+
+// Immediately Invoked Function Expressions (IIFE) 
+
+/* revent our code from leaking into the global scope is by wrapping it with a construct known as an 
+Immediately Invoked Function Expression, or “IIFE” (pronounced “iffy”). 
+It looks like this: 
+*/
+
+(function () {
+
+    'use strict';
+
+})(); 
+
+function nestedFunc(func){
+
+    function consoleMe(func2){
+        return func + func2;
+
+    }
+    return func2; 
+}
+
+
+// Nested functions
+
+function setUpItem(){
+
+    let myNumber = 5;
+
+    function increment(){
+        return console.log(myNumber++);
+    }
+    increment();
+    increment();
+    increment();
+    increment();
+    increment();
+    increment();
+
+}
+
+setUpItem()
